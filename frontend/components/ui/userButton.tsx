e4c4c187492f1a70efd7b5e1a1d4eae44dd833b6
Ishaan Dey <ishaan1013@gmail.com>
2024-04-28 01:17:39 -0400

    add signout logic

--- frontend/components/ui/userButton.tsx
+++ frontend/components/ui/userButton.tsx
@@ -9,10 +9,16 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { User } from "@/lib/types"
+import { useClerk } from "@clerk/nextjs"
 import { LogOut, Pencil } from "lucide-react"
+import { useRouter } from "next/navigation"
 
 export default function UserButton({ userData }: { userData: User }) {
   if (!userData) return null
+
+  const { signOut } = useClerk()
+  const router = useRouter()
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
@@ -30,7 +36,10 @@ export default function UserButton({ userData }: { userData: User }) {
           {/* open modal with name and email (disabled) */}
         </DropdownMenuItem>
         <DropdownMenuSeparator />
-        <DropdownMenuItem className="!text-destructive cursor-pointer">
+        <DropdownMenuItem
+          onClick={() => signOut(() => router.push("/"))}
+          className="!text-destructive cursor-pointer"
+        >
           <LogOut className="mr-2 h-4 w-4" />
           <span>Log Out</span>
         </DropdownMenuItem>
