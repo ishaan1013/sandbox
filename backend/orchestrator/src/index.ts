8b8bed75ded15ea906768acec43e7a8e53ff1d5b
Ishaan Dey <ishaan1013@gmail.com>
2024-05-13 16:05:08 -0700

    remove gcloud package

--- backend/orchestrator/src/index.ts
+++ backend/orchestrator/src/index.ts
@@ -12,7 +12,6 @@ import {
   NetworkingV1Api,
 } from "@kubernetes/client-node"
 import { z } from "zod"
-import container from '@google-cloud/container';
 
 const app = express()
 const port = process.env.PORT || 4001
@@ -109,25 +108,25 @@ app.post("/test", async (req, res) => {
 app.post("/start", async (req, res) => {
   const { sandboxId } = dataSchema.parse(req.body)
 
-  async function main() {
-    const client = new container.v1.ClusterManagerClient();
+  // async function main() {
+  //   const client = new container.v1.ClusterManagerClient();
   
-    async function quickstart() {
-      const zone = 'us-central1-a';
-      const projectId = await client.getProjectId();
-      const request = {
-        projectId: projectId,
-        zone: zone,
-      };
+  //   async function quickstart() {
+  //     const zone = 'us-central1-a';
+  //     const projectId = await client.getProjectId();
+  //     const request = {
+  //       projectId: projectId,
+  //       zone: zone,
+  //     };
   
-      const [response] = await client.listClusters(request);
-      console.log('Clusters: ', response);
-    }
+  //     const [response] = await client.listClusters(request);
+  //     console.log('Clusters: ', response);
+  //   }
 
-    quickstart();
-  }
+  //   quickstart();
+  // }
 
-  main().catch(console.error);
+  // main().catch(console.error);
 
   try {
 
